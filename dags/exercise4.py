70ef72a2cefc2e4d5d7e24af3becd438604f1c0e
cg-mihabatic <44840464+cg-mihabatic@users.noreply.github.com>
2019-11-22 15:14:50 +0100

    Update exercise4.py

--- dags/exercise4.py
+++ dags/exercise4.py
@@ -21,7 +21,7 @@ pgsl_to_gcs = PostgresToGoogleCloudStorageOperator(
   task_id="transfer_data_from_postgres",
   sql="SELECT * FROM land_registry_price_paid_uk WHERE transfer_date = '{{ ds }}'",
   bucket="airflow-training-data-1983",
-  filename=”{{ ds }}/properties_{}.json", 
+  filename="{{ ds }}/properties_{}.json", 
   postgres_conn_id="GoogleCloudSQL-miha",
   dag=dag,
 )
